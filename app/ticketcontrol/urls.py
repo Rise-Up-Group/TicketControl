1a9ca182dc0014084415800b5b2e1d71a758a5d1
ransbachm <ransbachm@gmail.com>
2022-03-21 18:33:48 +0100

    Adjust project settings to new environment

--- app/ticketcontrol/urls.py
+++ app/ticketcontrol/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import *
 
 app_name="ticketcontrol"
@@ -32,3 +34,9 @@ urlpatterns = [
 ]
 
 handler404 = "ticketcontrol.views.handler404"
+
+if settings.DEBUG:
+    urlpatterns += static (
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
