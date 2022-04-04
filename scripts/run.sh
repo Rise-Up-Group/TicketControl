3ed28b26aca70056363b10127062f7a6ead463e6
ransbachm <ransbachm@gmail.com>
2022-04-04 19:56:37 +0200

    Fix docker compose 3 (#10)
    * Fix dockerfile path
    
    * Add warning to run.sh
    
    * Fix and tidy up docker files
    
    * Delete unnecessary migration

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# remenber to also change the run command in the dev compose file
 while ! python manage.py sqlflush > /dev/null 2>&1 ;do
   echo "Waiting for the db to be ready."
   sleep 1
