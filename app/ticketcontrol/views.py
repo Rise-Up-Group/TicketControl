02f529c44d89ebf705e5a40e181cb75e3ed3a886
Comet1903 <github@login.julian-horn.net>
2022-05-16 14:28:32 +0200

    remove duplicate function

--- app/ticketcontrol/views.py
+++ app/ticketcontrol/views.py
@@ -542,6 +542,7 @@ def ticket_status_update(request, id):
             return HttpResponse(status=409)
     return HttpResponse(get_token(request))
 
+
 @permission_required("ticketcontrol.hide_ticket")
 def ticket_hide(request, id):
     if request.method == "POST":
@@ -555,18 +556,6 @@ def ticket_hide(request, id):
             return HttpResponse(status=409)
     return HttpResponse(get_token(request))
 
-@permission_required("ticketcontrol.unhide_ticket")
-def ticket_unhide(request, id):
-    if request.method == "POST":
-        try:
-            ticket = Ticket.objects.get(id=id)
-            ticket.set_hidden(False)
-            return redirect("ticket_view", id=id)
-        except ObjectDoesNotExist:
-            return HttpResponse(status=404)
-        except DatabaseError:
-            return HttpResponse(status=409)
-    return HttpResponse(get_token(request))
 
 @permission_required("ticketcontrol.unhide_ticket")
 def ticket_unhide(request, id):
@@ -580,6 +569,7 @@ def ticket_unhide(request, id):
         except DatabaseError:
             return HttpResponse(status=409)
     return HttpResponse(get_token(request))
+
 
 @permission_required("ticketcontrol.delete_ticket")
 def ticket_delete(request, id):
