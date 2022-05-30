c88cce04a2a6ac811272bfefd365f8b0742731f2
Comet1903 <github@login.julian-horn.net>
2022-05-30 18:42:38 +0200

    extend email-whitelist

--- app/ticketcontrol/static/js/emailWhitelist.js
+++ app/ticketcontrol/static/js/emailWhitelist.js
@@ -5,16 +5,23 @@ function removeEmailWhitelistEntry(button) {
 function addEmailWhitelistEntry() {
     let div = document.createElement("div");
     let input = document.createElement("input");
+    let buttonDiv = document.createElement("div");
     input.name = "register.email-whitelist";
     input.type = "email";
+    input.classList.add("form-control");
+    buttonDiv.classList.add("input-group-append");
+    buttonDiv.classList.add("text-white");
     div.appendChild(input);
     div.innerHTML += " ";
+    div.classList.add("input-group");
+    div.classList.add("mb-3");
     let button = document.createElement("a");
     button.innerHTML = "Löschen";
     button.classList.add("btn");
-    button.classList.add("btn-primary");
+    button.classList.add("btn-danger");
     button.setAttribute("onclick", "removeEmailWhitelistEntry(this)");
-    div.appendChild(button);
+    buttonDiv.appendChild(button);
+    div.appendChild(buttonDiv)
     let emailWhitelist = document.getElementById("register.email_whitelist");
     emailWhitelist.appendChild(div);
     emailWhitelist.lastChild.firstChild.focus();
